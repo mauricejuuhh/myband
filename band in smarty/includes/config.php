c85c2997c3e43c55e8989799ff26ecc75ebb5b4f
mauricejuuhh <mauricejuuh3@gmail.com>
2017-06-19 09:32:52 +0200

    my band smarty

---
+++ band in smarty/includes/config.php
@@ -0,0 +1,10 @@
+<?php
+$title = 'MyBand';
+$footer = '© 2017 Maurice de Jong - MyBand';
+
+define('DB_HOST','localhost');
+define('DB_NAME','proj_myband');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+
+?>
